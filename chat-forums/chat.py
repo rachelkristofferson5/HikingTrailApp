ff5deb3b1a82b03d69a6e3002f42e44a4a76c767
AhmedHassancsp <hassana41@csp.edu>
2025-10-01 19:41:38 -0500

    setting up chat for python

---
+++ chat-forums/chat.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Chat(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
